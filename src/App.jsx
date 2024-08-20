ae4630f8ae41725ba48d8f4cd6a81dcafa36e8be
SnehaY20 <21snehay@gmail.com>
2024-08-20 23:00:14 +0530

    Built a Food Order app using react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
+import Cart from "./components/UI/Cart.jsx";
+import Checkout from "./components/UI/Checkout.jsx";
+import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
+
+function App() {
+  return (
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
+  );
+}
+
+export default App;
